4ccaa4f3bd47582e18b90f1f730f71bcc7887652
sadia-mehjabin <jhidnicraft@gmail.com>
2023-01-29 21:00:32 +0600

    final commit

--- rotate.js
+++ rotate.js
@@ -1,21 +1,3 @@
-// let arr = [3, 4, 5, 6, 7, 1, 2];
-// const input = 2;
-// let temporary = [];
-// let num = 0 ;
-// for(let i = 0; i < arr.length; i++){
-//     // 
-//     if(i >= input){
-//         temporary.push(arr[i])  
-//     }  
-// }
-// for(let i = 0; i < arr.length; i++){
-//     if(i < input){
-//         temporary.push(arr[i])  
-//     }  
-// }
-// arr = [...temporary]
-// console.log(arr)
-
 let temporary = [];
 function rotateLeft(array, number){
     for(let i = 0; i < array.length; i++){
